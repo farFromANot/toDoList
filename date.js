9d8d4d710c3258adef888372ae826bd52ae54a37
Gibson <IanGibson@clearchannel.com>
2020-02-07 14:01:49 -0600

    leard module.exports

---
+++ date.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+
+
+
+module.exports.getDate = function(){
+    const today = new Date();
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+        
+    }
+
+    let day = today.toLocaleDateString("en-US", options)
+    return day
+}
+
+
+
+module.exports.getDay = function(){
+    const today = new Date();
+    const options = {
+        weekday: "long",
+    }
+
+    let day = today.toLocaleDateString("en-US", options)
+    return day
+}
